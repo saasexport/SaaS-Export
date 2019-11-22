5495252e13fd73eac2d37a9a440994c5d34504aa
bian1721 <1721536603@qq.com>
2019-11-22 14:55:52 +0800

    默认更改列表

---
+++ export_parent/export_cargo_interface/src/main/java/cn/itcast/service/cargo/ShippingOrderService.java
@@ -0,0 +1,37 @@
+package cn.itcast.service.cargo;
+
+import cn.itcast.domain.cargo.ShippingOrder;
+import cn.itcast.domain.cargo.ShippingOrderExample;
+import com.github.pagehelper.PageInfo;
+
+
+import java.util.List;
+
+public interface ShippingOrderService {
+    void deleteByPrimaryKey(String shippingOrderId);
+
+    //int insert(ShippingOrder record);
+
+    void insertSelective(ShippingOrder record);
+
+    List<ShippingOrder> selectByExample(ShippingOrderExample example);
+
+    ShippingOrder selectByPrimaryKey(String shippingOrderId);
+
+    void updateByPrimaryKeySelective(ShippingOrder record);
+
+    //分页查询委托单
+    PageInfo findAll(ShippingOrderExample shippingOrderExample, int page, int size);
+
+    //保存委托单,id是装箱单id
+    void save(ShippingOrder shippingOrder);
+
+    //更新委托单,id是装箱单id
+    void update(ShippingOrder shippingOrder);
+
+    //更新之前回显委托单数据
+    ShippingOrder findById(String shippingOrderId);
+
+    //删除委托单
+    void delete(String shippingOrderId);
+}
