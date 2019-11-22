b9e32344b6fc1c5e997fb503a9b4c6c11b723993
bian1721 <1721536603@qq.com>
2019-11-22 11:23:09 +0800

    默认更改列表

---
+++ export_parent/export_dao/src/main/java/cn/itcast/dao/cargo/ShippingOrderDao.java
@@ -0,0 +1,24 @@
+package cn.itcast.dao.cargo;
+
+
+
+import cn.itcast.domain.cargo.ShippingOrder;
+import cn.itcast.domain.cargo.ShippingOrderExample;
+
+import java.util.List;
+
+public interface ShippingOrderDao {
+    //删除委托单
+    int deleteByPrimaryKey(String shippingOrderId);
+    //添加委托单
+    int insert(ShippingOrder record);
+    //添加委托单
+    int insertSelective(ShippingOrder record);
+    //查询委托单列表
+    List<ShippingOrder> selectByExample(ShippingOrderExample example);
+    //查询委托单列表
+    ShippingOrder selectByPrimaryKey(String shippingOrderId);
+    //更新委托单信息
+    int updateByPrimaryKeySelective(ShippingOrder record);
+
+}
